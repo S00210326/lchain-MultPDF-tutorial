640fa43ec2c648a2315f6e649dde0704ad951330
S00210326 <s00210326@atu.ie>
2023-06-28 08:44:13 +0200

    working but need to fix display

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from htmlTemplates import css, bot_template, user_template
 import os
 
 load_dotenv()
@@ -29,6 +33,15 @@ def get_text_chunks(text):
     return chunks
 
 
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, retriever=vectorstore.as_retriever(), memory=memory
+    )
+    return conversation_chain
+
+
 # using OpenAI embeddings and then storing using FAISS(could be swapped for pinecone)
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
@@ -36,10 +49,43 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 
+# when user submits a question
+def handle_userinput(user_question):
+    if st.session_state.conversation is not None:  # Add this check
+        response = st.session_state.conversation({"question": user_question})
+        st.session_state.chat_history = response["chat_history"]
+
+        for i, message in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                st.write(
+                    user_template.replace("{{MSG}}", message.content),
+                    unsafe_allow_html=True,
+                )
+            else:
+                st.write(
+                    bot_template.replace("{{MSG}}", message.content),
+                    unsafe_allow_html=True,
+                )
+    else:
+        st.write("Please process the PDFs first before asking a question.")
+
+
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    # allows to use css from the imported template
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("Chat with multiple PDFs :books:")
-    st.text_input("Ask a question about your docs: ")
+    user_question = st.text_input("Ask a question about your docs: ")
+    if user_question:
+        handle_userinput(user_question)
+    # replacing personalized template, loading straight html code
 
     with st.sidebar:
         st.subheader("Your Documents")
@@ -56,6 +102,11 @@ def main():
 
                 # create vector store with embeddings
                 vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                # this allows you to get history of conversation and returns you the next element
+                # Needs to be persistant(Especially for using STREAMLIT where it re-initialises alot)
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
 if __name__ == "__main__":
