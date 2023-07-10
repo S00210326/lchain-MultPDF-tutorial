1210717d902108dd88d6f0eab7bfeb90647e2461
S00210326 <s00210326@atu.ie>
2023-07-10 15:07:42 +0200

    added encoding and plotted

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 # import streamlit as st
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
 
-from langchain.text_splitter import CharacterTextSplitter
+# from PyPDF2 import PdfReader
+
+# from langchain.text_splitter import CharacterTextSplitter
 from langchain.indexes import GraphIndexCreator
 from langchain.llms import OpenAI
-from langchain.document_loaders import TextLoader, Docx2txtLoader
+
+# from langchain.document_loaders import TextLoader, Docx2txtLoader
 import networkx as nx
 import matplotlib.pyplot as plt
-from langchain.graphs import GraphCypherQAChain
-from langchain.chains import ConversationalRetrievalChain, GraphQAChain
+from langchain.chains import (
+    ConversationalRetrievalChain,
+    GraphQAChain,
+    GraphCypherQAChain,
+)
+from langchain.graphs.networkx_graph import NetworkxEntityGraph, KnowledgeTriple
+from langchain.graphs import NetworkxEntityGraph
+
+# import openai
+import tiktoken
+
+# import nltk
+
 from langchain.chat_models import ChatOpenAI
 
 # from langchain.chat_models import ChatOpenAI
@@ -24,61 +37,108 @@ PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_API_ENV = os.getenv("PINECONE_API_ENV")
 
 
-def get_pdf_text(pdf_doc):
-    text = ""
-    # Reads all pages of the PDF and gets all text
-    pdf_reader = PdfReader(pdf_doc)
-    for page in pdf_reader.pages:
-        text += page.extract_text()
-    return text
+def clean_text(cleaned_text):
+    # cleaned_text = " ".join([word for word in cleaned_text.split() if word.isalpha()])
+
+    return cleaned_text
 
 
-def get_text_chunks(text):
-    text_spliter = CharacterTextSplitter(
-        separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
-    )
-    chunks = text_spliter.split_text(text)
+def chunk_text_by_tokens(text, max_tokens=3000):
+    """
+    Splits the text into chunks of a specified number of tokens.
+    """
+    encoding = tiktoken.get_encoding("cl100k_base")
+    tokens = encoding.encode(text)
+    chunks = []
+
+    for i in range(0, len(tokens), max_tokens):
+        chunk_tokens = tokens[i : i + max_tokens]
+        chunk_text = encoding.decode(chunk_tokens)
+        chunks.append(chunk_text)
+
     return chunks
 
 
-pdf_text = get_pdf_text("ALAllints2.pdf")
+# def get_pdf_text(pdf_doc):
+#     text = ""
+#     # Reads all pages of the PDF and gets all text
+#     pdf_reader = PdfReader(pdf_doc)
+#     for page in pdf_reader.pages:
+#         text += page.extract_text()
+#     return text
+
+
+# def get_text_chunks(text):
+#     text_spliter = CharacterTextSplitter(
+#         separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
+#     )
+#     chunks = text_spliter.split_text(text)
+#     return chunks
+
+
+with open("ALAllints2.txt") as f:
+    all_text = f.read()
+
+
+# text = "\n".join(all_text.split("\n"))
+cleaned_text = clean_text(all_text)
+
+# print(cleaned_text)
+# pdf_text = get_pdf_text("ALAllints2.pdf")
 
 # text = get_text_chunks(pdf_text)
+openai_llm = OpenAI(temperature=0)
 
 index_creator = GraphIndexCreator(llm=OpenAI(temperature=0))
 
 
-graph = index_creator.from_text(pdf_text)
+# Split the text into chunks
+chunks = chunk_text_by_tokens(cleaned_text)
 
-graph.get_triples()
-graph_chain = GraphCypherQAChain(graph)
+# Initialize an empty graph
 
-chain = GraphQAChain.from_llm(OpenAI(temperature=0), graph=graph, verbose=True)
-question = "What is the relationship between verbose and ALSTOM "
-
-chain.run(question)
-
-conversation = ChatOpenAI()
-response = conversation.send_message("How would I converse with the graph database?")
-answer = graph_chain.run(response)
-
-
-# Create graph
 G = nx.DiGraph()
-G.add_edges_from(
-    (source, target, {"relation": relation})
-    for source, relation, target in graph.get_triples()
-)
 
+# Process each chunk
+for chunk in chunks:
+    # Create a graph index for the chunk
+    chunk_graph = index_creator.from_text(chunk)
+    triples = chunk_graph.get_triples()
+
+    # Add triples to your DiGraph
+    for triple in triples:
+        G.add_edges_from([(triple[0], triple[2], {"relation": triple[1]})])
+# graph = index_creator.from_text(cleaned_text)
+# for edge in G.edges(data=True):
+#     print((edge[0], edge[2]["relation"], edge[1]))
+# chunk_graph.get_triples()
+# print(G)
+
+entity_graph = NetworkxEntityGraph(G)
+triples = entity_graph.get_triples()
+
+print(triples)
+chain = GraphQAChain.from_llm(openai_llm, graph=entity_graph, verbose=True)
+question = "Who owns alstom and what is it ? "
+
+print(chain.run(question))
+
+# # Create graph
+# G = nx.DiGraph()
+# G.add_edges_from(
+#     (source, target, {"relation": relation})
+#     for source, relation, target in graph.get_triples()
+# )
+# G = nx.DiGraph()
 # Plot the graph
-plt.figure(figsize=(8, 5), dpi=300)
+plt.figure(figsize=(8, 5), dpi=500)
 pos = nx.spring_layout(G, k=3, seed=0)
 
-nx.draw_networkx_nodes(G, pos, node_size=1000)
+nx.draw_networkx_nodes(G, pos, node_size=150)
 nx.draw_networkx_edges(G, pos, edge_color="gray")
-nx.draw_networkx_labels(G, pos, font_size=6)
+nx.draw_networkx_labels(G, pos, font_size=3)
 edge_labels = nx.get_edge_attributes(G, "relation")
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=10)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=4)
 
 # Display the plot
 plt.axis("off")
